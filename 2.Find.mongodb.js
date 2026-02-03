a0c1e21693e9f28cee3d5bd77eb81ae33af70317
FehFialho <fernandaklechowicz@gmail.com>
2026-02-03 09:23:29 -0300

    Regex

--- 2.Find.mongodb.js
+++ 2.Find.mongodb.js
@@ -4,4 +4,16 @@
 // The current database to use.
 use("bosch");
 
-db.people.find()
+// Just Like SQL Where
+//db.people.find({name: 'Trevisan'})
+
+
+// REGEX - Sequencia de caracteres que forma um padrão de busca.
+// Utilizada para buscar, substituir ou validar texto de acordo com padrões.
+// Like do SQL.
+
+//db.people.find({name: /n/ }) //Contem N
+
+//db.people.find({name: /^D.*n$/ }) // Começa com D e termina com N
+
+db.people.find($and [{ name: 'Cesar'}, { lastname: 'Stati'}] ) // And SQL
