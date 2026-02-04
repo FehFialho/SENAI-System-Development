9ab932eb7e329a1ebcf4331079c53c790aac37c0
FehFialho <fernandaklechowicz@gmail.com>
2026-02-04 08:30:40 -0300

    Regex Search

--- 0.Lesson/crud.mongodb.js
+++ 0.Lesson/crud.mongodb.js
@@ -1,3 +1,13 @@
 use('challenge')
 
-db.users.find({nome: /^A/i}) // Começa com A
+// #### REGEX SEARCH ####
+
+db.users.find({nome: /^A/i}) // Starts with A
+
+db.users.find({telefone: /^9/}) // Starts with 9
+
+db.users.find({email: /@gmail.com/}) // Has @gmail.com
+
+db.users.find({cidade: /São Paulo/}) // From SP
+
+db.users.find({dataCadastro: {$gt: new Date("2023-01-01")}}) // Date Before 2023
