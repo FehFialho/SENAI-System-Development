69803c267e1a802f291901f909e54e211c71d554
FehFialho <fernandaklechowicz@gmail.com>
2026-02-04 08:40:08 -0300

    DB Update

--- 0.Lesson/crud.mongodb.js
+++ 0.Lesson/crud.mongodb.js
@@ -11,3 +11,26 @@ db.users.find({email: /@gmail.com/}) // Has @gmail.com
 db.users.find({cidade: /São Paulo/}) // From SP
 
 db.users.find({dataCadastro: {$gt: new Date("2023-01-01")}}) // Date Before 2023
+
+// #### UPDATE DATA ####
+use('challenge')
+db.users.find({nome: /^C/}) // Starts with C
+
+db.users.updateOne(
+    { nome: 'Carlos Pereira'},
+    { $set: { 
+        telefone: '41986371634',
+        email: 'carlos@nova.empresa.com'
+        }
+    }
+)
+
+db.users.updateOne(
+    { nome: 'Daniel Rocha'},
+    { $set: { cidade: 'Araucária' }}
+)
+
+db.users.updateMany(
+    { telefone: /^41/ },
+    { $set: { cidade: 'Curitiba' }}
+)
