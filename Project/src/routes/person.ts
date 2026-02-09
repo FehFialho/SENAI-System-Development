bdd3cf3bad3d19eae7492168032d10cbdfee326f
FehFialho <fernandaklechowicz@gmail.com>
2026-02-09 11:43:29 -0300

    Post and Get Users

--- Project/src/routes/person.ts
+++ Project/src/routes/person.ts
@@ -3,6 +3,18 @@ import express, { Request, Response, Router } from 'express';
 const router: Router = express.Router();
 const people: object[] = [];
 
-//....
+router
+    .post('/registrar', (req: Request, res: Response) => {
+        const { name, lastname } = req.body
+        console.log(name, lastname)
+        people.push({name, lastname})
+        res.status(200).send({ message: `Usuário ${name} ${lastname} registrado com sucesso!`})
+    })
+
+    .get('/usuarios', (req: Request, res: Response) => {
+        res.status(200).send({ users: people})
+    })
+
+//...
 
 export default router;
