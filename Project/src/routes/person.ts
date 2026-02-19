327615914b0fe466b8b1e5d69c1b2c7588da6d83
FehFialho <fernandaklechowicz@gmail.com>
2026-02-19 11:41:36 -0300

    Delete

--- Project/src/routes/person.ts
+++ Project/src/routes/person.ts
@@ -1,14 +1,16 @@
 import express, { Request, response, Response, Router } from 'express';
 
 const router: Router = express.Router();
-const people: object[] = [];
+//const people: object[] = [];
+const people: any[] = []
+
 
 router
     .post('/registrar', (req: Request, res: Response) => {
-        const { name, lastname } = req.body
+        const { name, lastname, id } = req.body
         console.log(name, lastname)
-        people.push({name, lastname})
-        res.status(200).send({ message: `Usuário ${name} ${lastname} registrado com sucesso!`})
+        people.push({name, lastname, id})
+        res.status(200).send({ message: `Usuário ${name} ${lastname} registrado com sucesso [ID - ${id}]!`})
     })
 
     .get('/usuarios', (req: Request, res: Response) => {
@@ -29,8 +31,22 @@ router
    .put('/atualizar/:id', (req: Request, res: Response) => {
         const {id} = req.params
         const {name,lastname} = req.body
+        res.status(200).send({ response: `Atualizando usuário ${id} -> ${name} ${lastname}` })
    })
 
+    .delete('/deletar/:id', (req: Request, res: Response) => {
+        const id = Number(req.params.id)
+
+        const index = people.findIndex(p => p.id === id)
+
+        if (index === -1) {
+            return res.status(404).send("Usuário não encontrado")
+        }
+
+        people.splice(index, 1)
+
+        res.status(200).send("Usuário deletado com sucesso")
+    })
 //...
 
 export default router;
