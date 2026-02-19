c6b0677cf5fab2afcf6a6e30997ac6ac6a28dcf0
FehFialho <fernandaklechowicz@gmail.com>
2026-02-19 10:58:57 -0300

    Update

--- Project/src/routes/person.ts
+++ Project/src/routes/person.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router } from 'express';
+import express, { Request, response, Response, Router } from 'express';
 
 const router: Router = express.Router();
 const people: object[] = [];
@@ -14,6 +14,22 @@ router
     .get('/usuarios', (req: Request, res: Response) => {
         res.status(200).send({ users: people})
     })
+
+    .get('/usuarios/:id', (req: Request, res: Response) => {
+        const { id } = req.params
+        let convertedId = Number(id)
+       res.status(200).send({ response: people[convertedId] })
+   })
+
+    .get('/filtro', (req: Request, res: Response) => {
+        const { name, lastname } = req.query
+       res.status(200).send({ response: `${name}, ${lastname}` })
+   })
+
+   .put('/atualizar/:id', (req: Request, res: Response) => {
+        const {id} = req.params
+        const {name,lastname} = req.body
+   })
 
 //...
 
