0f8fbce9e855d6f50fc4bd589b13d3e88b3c286e
FehFialho <fernandaklechowicz@gmail.com>
2026-02-24 09:37:35 -0300

    Get Users

--- B. Academy/src/routes/user.ts
+++ B. Academy/src/routes/user.ts
@@ -5,15 +5,39 @@ const router = express.Router();
 // Simulando banco de dados
 let users: any[] = []
 
-router.post('/register', (req: Request, res: Response) => {
-const { name, email, type } = req.body
-    console.log(name, email)
-    const id = users.length + 1
-    const isActive = true
-    const createdAt = new Date().toISOString()
-    users.push({name, email, type, isActive, createdAt, id})
-    res.status(201).send({ message: `Usuário ${name} registrado com sucesso [ID - ${id}]!`})
-})
+// Registrar usuários
+router
+    .post('/register', (req: Request, res: Response) => {
+        const { name, email, type } = req.body
+
+        // Verificação!
+        if (!name || !email || !type) {
+            return res.status(400).send({
+                message: "Campos obrigatórios: name, email e type"
+            })
+        }
+
+        const id = users.length + 1 // Id Auto
+        const isActive = true // Criou agora, deve estar ativo ne mkkk
+        const createdAt = new Date().toISOString() // Tempo de agora
+
+        users.push({ name, email, type, isActive, createdAt, id })
+
+        res.status(201).send({
+            message: `Usuário ${name} registrado com sucesso [ID - ${id}]!`
+        })
+    })
+
+    .get('/get', (req: Request, res: Response) => {
+        res.status(200).send({ users: users})
+
+        if (users.length == 0)
+        {
+            return res.status(404).send({
+                message: "Erro: Nenhum usuário registrado!"
+            })    
+        }
+    })
 
 export default router;
 
