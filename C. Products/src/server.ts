ea3deff7e1781394aa16c7088d2696fca9ec85ea
FehFialho <fernandaklechowicz@gmail.com>
2026-03-03 11:37:10 -0300

    Fix config folder

--- C. Products/src/server.ts
+++ C. Products/src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
 import connectDB from './database/database';
+import routes from './routes/routes';
 // import routes from "./routes/routes.ts";
 
 const app = express();
 const port = 3000;
 
 connectDB();
+routes(app);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Olá, mundo!');
