9718b43a5433fbc8ad1e9d936acdb420f0d7b483
FehFialho <fernandaklechowicz@gmail.com>
2026-03-10 09:25:00 -0300

    Get and Show Products

--- C. Products/product-catalog/src/App.tsx
+++ C. Products/product-catalog/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, type JSXElementConstructor, type ReactElement, type ReactNode, type ReactPortal } from 'react'
 import './App.css'
 import axios from 'axios'
 
@@ -7,9 +7,12 @@ function App() {
   const [products, setProducts] = useState<any>([])
 
   const fetchData = async () => {
-    const data = await axios.get("http://localhost:8080/api/products")
-    setProducts(data)
-    console.log(data)
+    const response = await axios.get("http://localhost:8080/api/products")
+
+    const products = response.data.products
+
+    setProducts(products)
+    console.log(products)
   }
 
   useEffect(() => {
@@ -18,9 +21,38 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-sky-600 text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <section className='bg-cyan-500 flex w-screen h-screen justify-around items-center p-4'>
+        {/* Products */}
+        <section className='bg-amber-100/10 w-14/20 h-auto p-6 flex flex-row flex-wrap justify-around items-center gap-8 rounded-lg'>
+        {
+          products.map((product: any) => (
+            <div key={product._id} className='bg-gray-100/90 w-100 h-70 p-5 rounded-lg flex flex-col'>
+              
+              <span className='font-bold text-gray-700 text-2xl'>
+                {product.name}
+              </span>
+
+              <span className='text-gray-600'>
+                {product.description}
+              </span>
+
+              <span className='text-gray-700'>
+                Categoria: {product.category}
+              </span>
+
+              <span className='text-gray-700 font-semibold'>
+                R$ {product.price}
+              </span>
+
+              <span className='text-gray-500'>
+                Estoque: {product.stock}
+              </span>
+
+            </div>
+          ))
+        }
+        </section>
+      </section>
     </>
   )
 }
